5ef72c5b85fa5d221ac2b3b6ed544d6233ebfbc8
MuhireIghor <muhireighor123@gmail.com>
2022-11-18 23:23:17 +0200

    workplan responsive.jsx

--- apptest/src/components/WorkPlan.jsx
+++ apptest/src/components/WorkPlan.jsx
@@ -1,7 +1,7 @@
 import MenStaff from '../assets/nhnMenStaff.png'
 const WorkPlace = ()=>{
     return(
-        <div className="flex items-between justify-between h-[32rem] bg-[#E5E5E5] gap-4 p-4">
+        <div className="flex items-between justify-between h-[32rem] bg-[#E5E5E5]  gap-12 p-4">
             <div className='bg-[#C4C4C4] flex flex-col items-center justify-center relative'>
 <div className="flex flex-col w-full  bg-black items-center justify-center -mr-16 p-12 gap-8 ">
 <p className="text-white">LET'S  WORK  TOGETHER</p>
@@ -10,7 +10,7 @@ const WorkPlace = ()=>{
 </div>
             </div>
 <div className="flex flex-col w-full h-full items-end ">
-    <img src={MenStaff} className="w-2/3 h-full" />
+    <img src={MenStaff} className="h-full" />
 </div>
         </div>
     )
